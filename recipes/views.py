e027bda196ca1b3161096d04ce0b01cf40a03491
Guilherme Martins Andrade <guilherme.m.andrade@edu.ufes.br>
2025-01-29 21:48:49 -0300

    init recipes web-site

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('Contato 1')
-
-def sobre(request):
-    return HttpResponse('Sobre 1')
+    return render(request, 'recipes/home.html', context={'name': 'Guilherme'})
