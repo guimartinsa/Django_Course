5322eedb048134ba1579eddb8ca03a0384c6f676
Guilherme Martins Andrade <guilherme.m.andrade@edu.ufes.br>
2025-01-30 01:05:09 -0300

    add head,header, share and main-content

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Guilherme'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Guilherme'})
