f9c45c5e2cf939555040008016f40146c5c5275f
Guilherme Martins Andrade <guilherme.m.andrade@edu.ufes.br>
2025-02-05 03:32:22 -0300

    add info bd

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=165)
+    slug = models.SlugField()
+    prepation_time = models.IntegerField()
+    prepation_time_unit = models.CharField( max_length=60)
+    servings = models.IntegerField()
+    servings_unit = models.CharField( max_length=60)
+    prepation_steps = models.TextField()
+    prepation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    categoty = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
