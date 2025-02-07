fd845dbf265a043873370573140bb73514a559ed
Guilherme Martins Andrade <guilherme.m.andrade@edu.ufes.br>
2025-02-07 17:30:24 -0300

    title and 404-error

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes': recipes,})
+    recipes = Recipe.objects.filter(is_published = True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+    'recipes': recipes,                                                         
+})
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={'recipes': recipes,})
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found 🥲')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title_category': f'{recipes.first().category.name} - Category |',
+    })
 
 
 
